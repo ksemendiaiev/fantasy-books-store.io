49cdbbf307352b5a6afbffcbeb261ca25041b6e2
Kirill Semendiaiev <124810041+ksemendiaiev@users.noreply.github.com>
2025-07-07 00:42:55 +0200

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "https://fantasy-books-storeio-production.up.railway.app/api/v1";
+//const API_URL = "https://fantasy-books-storeio-production.up.railway.app/api/v1";
+const API_URL = "http://localhost:1000/api/v1";
+
 
 const api = axios.create({
     baseURL: API_URL,
